6c4a4f277e060b7ceddd2a068a2fa390c6ba3d78
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2024-10-30 19:08:33 +0000

    second commit

--- index.js
+++ index.js
@@ -45,6 +45,60 @@ app.get("/", async (req, res) => {
     }
 })
 
+app.get("/search", async (req, res) => {
+    const searchInput = req.query.search
+    const apiRequest = await axios.get(APIurl + "?q=" + searchInput + "&printType=books&startIndex=0&maxResults=15")
+    const result = apiRequest.data.items //apiRequest.data.items for nested objects content
+    //const bookInfo = result[0].volumeInfo
+    //const searchInfo = result[0].searchInfo
+    //const thumbnails = result[0].volumeInfo.imageLinks
+    //const googleId = result[0].id
+
+    //console.log("Google ID:");
+    //console.log(googleId);
+    //console.log("result:");
+    //console.log(result);
+    //console.log("bookInfo:");    
+    //console.log(bookInfo);  
+
+    res.render("search.ejs", {
+        books: result,
+        //apiId: result.id,
+        //volumeInfo: bookInfo,
+        //textSnippet: searchInfo.textSnippet,
+        //thumbnails: thumbnails
+    })
+})
+
+app.post("/add", async (req, res) => {
+    const bookId = req.body.bookApiId
+    try {
+        const result = await axios.get(APIurl + "/" + bookId)
+        const bookInfo = result.data 
+
+        res.render("input.ejs", {
+            book: bookInfo
+        })
+    } catch (error) {
+        console.log(error)
+        console.log("Error fetching from API")
+    }
+})
+
+app.post("/add-toList", async (req, res) => {
+    
+        try {
+            const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : "(n/a)";
+            await db.query("INSERT INTO books (book_name, author) VALUES ($1, $2)",
+            [book.volumeInfo.title, authors]);
+
+            res.redirect("/")
+        } catch (error) {
+            console.log(error)
+            console.log("Error saving into database")
+        }    
+})
+
 //Port
 app.listen(port, () =>{
     console.log("Server running at http://localhost:" + port);
